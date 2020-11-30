6a2971c21063f3258b860ba701e3511a38c54dd3
Lucypher66 <51540565+Lucypher66@users.noreply.github.com>
2020-11-30 19:16:56 +0100

    Add files via upload

--- archdeploy.py
+++ archdeploy.py
@@ -16,7 +16,7 @@ def systeminstallation():
    harddrive = input("Type the device path of the hard drive (/dev/sdX): ")
    mountstring = "mount " + harddrive + " /mnt"
    os.system(mountstring)
-   pacstrapstring = "pacstrap /mnt linux linux-firmware dhcpcd nano bash-completion python "
+   pacstrapstring = "pacstrap /mnt linux linux-firmware dhcpcd nano bash-completion python grub "
 
 
    option = input("Install the base-devel package? Mandatory if you want to build your own packages or use the AUR. Y/N: ")
@@ -69,9 +69,68 @@ def harddriveformat():
 def chrootinstall():
     hostnamestring = input("Enter the hostname that this device should have later: ")
     hostnamestring = "echo " + hostnamestring + " > /etc/hostname"
-    langstring = input("Enter your locale String (example: de_DE for germany): ")
-    langstringtopaste = langstring + ".UTF-8"
+    legacylibs = input("Should 32-bit libraries be supported? Y/N ")
+    uefi = input("Do you use UEFI? Y/N ")
+    harddrive = input("Type the device path of the hard drive where the bootlaoder will be installed (/dev/sdX): ")
+    os.system("passwd")
+    if legacylibs == "Y" or legacylibs == "y":
+        legacylibs = True
+    else:
+        legacylibs = False
+    if uefi == "Y" or uefi == "y":
+        uefi = True
+    else:
+        uefi = False
+
+    langstring = input("Enter your locale String (example: de_DE for germany), leave blank for USA (en_US): ")
+    if langstring == "":
+        langstring = "en_US"
+    langstringutf = langstring + ".UTF-8"
+
+    timezonestring = input("Enter your Timezone (for example Europe/Berlin): ")
+    timezonestring = "ln -sf /usr/share/zoneinfo/" + timezonestring + " /etc/localtime"
+
     os.system(hostnamestring)
+
+    os.system("rm /etc/locale.gen")
+    os.system("touch /etc/locale.gen")
+    langpaste = "echo " + langstring + " UTF-8 >> /etc/locale.gen"
+    os.system(langpaste)
+    langpaste = "echo " + langstring + " ISO-8859-1 >> /etc/locale.gen"
+    os.system(langpaste)
+    langpaste = "echo " + langstring + "@euro ISO-8859-15 >> /etc/locale.gen"
+    os.system(langpaste)
+    print("")
+    os.system("locale-gen")
+
+    if langstring == "de_DE":
+        os.system("KEYMAP=de-latin1 > /etc/vconsole.conf")
+    else:
+        os.system("KEYMAP=us")
+
+    os.system("echo FONT=lat9w-16 >> /etc/vconsole.conf")
+
+    if legacylibs == True:
+        os.system("echo [multilib] >> /etc/pacman.conf")
+        os.system("echo SigLevel = PackageRequired TrustedOnly >> /etc/pacman.conf")
+        os.system("echo Include = /etc/pacman.d/mirrorlist >> /etc/pacman.conf")
+        os.system("pacman -Sy")
+    else:
+        pass
+
+    os.system("mkinitcpio -p linux")
+
+
+    if uefi == True:
+        os.system("pacman -S efibootmgr")
+        os.system("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Grub2 ")
+    else:
+        bootloaderstring = "grub-install " + harddrive
+        os.system(bootloaderstring)
+
+    os.system("grub-mkconfig -o /boot/grub/grub.cfg")
+
+
     #os.system("cat /etc/hostname")
     packagestring = "pacman -S "
 
